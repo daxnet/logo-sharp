e51cefef031cb7905ca835fad44cbc86af0359ac
daxnet <daxnet@outlook.com>
2017-10-19 22:00:46 +0800

    Added the main form for the Logo IDE.

---
+++ src/LogoSharp.Main/FrmMain.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace LogoSharp.Main
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.textEditor1 = new LogoSharp.Main.TextEditor();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBuild = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuRun = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.programToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            // 
+            // toolStrip1
+            // 
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            // 
+            // splitContainer2
+            // 
+            resources.ApplyResources(this.splitContainer2, "splitContainer2");
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.elementHost1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pnlCanvas);
+            // 
+            // pnlCanvas
+            // 
+            resources.ApplyResources(this.pnlCanvas, "pnlCanvas");
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Name = "pnlCanvas";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // elementHost1
+            // 
+            resources.ApplyResources(this.elementHost1, "elementHost1");
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Child = this.textEditor1;
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            resources.ApplyResources(this.viewToolStripMenuItem, "viewToolStripMenuItem");
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuBuild,
+            this.mnuRun});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            resources.ApplyResources(this.programToolStripMenuItem, "programToolStripMenuItem");
+            // 
+            // mnuBuild
+            // 
+            this.mnuBuild.Name = "mnuBuild";
+            resources.ApplyResources(this.mnuBuild, "mnuBuild");
+            // 
+            // mnuRun
+            // 
+            this.mnuRun.Name = "mnuRun";
+            resources.ApplyResources(this.mnuRun, "mnuRun");
+            // 
+            // FrmMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Shown += new System.EventHandler(this.FrmMain_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Panel pnlCanvas;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private TextEditor textEditor1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuBuild;
+        private System.Windows.Forms.ToolStripMenuItem mnuRun;
+    }
+}
---
+++ src/LogoSharp.Main/FrmMain.cs
@@ -0,0 +1,106 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using LogoSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LogoSharp.Main
+{
+    public partial class FrmMain : Form
+    {
+        private readonly Turtle turtle;
+        private readonly Logo logo = new Logo();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            this.turtle = new Turtle(this.pnlCanvas);
+            this.turtle.SetPenWidth(2.0F);
+            this.turtle.Reset();
+
+            this.BindLogoEvents();
+
+            using (var stream = this.GetType().Assembly.GetManifestResourceStream("LogoSharp.Main.LogoSharp.xshd"))
+            {
+                using (var xmlTextReader = new XmlTextReader(stream))
+                {
+                    this.TextEditorInstance.SyntaxHighlighting = HighlightingLoader.Load(xmlTextReader, HighlightingManager.Instance);
+                }
+            }
+        }
+
+        private void BindLogoEvents()
+        {
+            logo.TurnLeft += (s, e) =>
+            {
+                this.turtle.Left(e.Angle);
+            };
+
+            logo.TurnRight += (s, e) =>
+            {
+                this.turtle.Right(e.Angle);
+            };
+
+            logo.Forward += (s, e) =>
+            {
+                this.turtle.MoveForward(e.Steps);
+            };
+
+            logo.Backward += (s, e) =>
+            {
+                this.turtle.MoveBackward(e.Steps);
+            };
+
+            logo.PenUp += (s, e) =>
+            {
+                this.turtle.PenStatus = PenStatus.Up;
+            };
+
+            logo.PenDown += (s, e) =>
+            {
+                this.turtle.PenStatus = PenStatus.Down;
+            };
+
+            logo.SetPenColor += (s, e) =>
+            {
+                this.turtle.SetPenColor(e.R, e.G, e.B);
+            };
+
+            logo.Delay += (s, e) =>
+            {
+                this.turtle.SetDelayMilliseconds(e.Milliseconds);
+            };
+
+            logo.ClearScreen += (s, e) =>
+            {
+                this.turtle.Clear();
+            };
+        }
+
+        private string SourceCode
+        {
+            get { return TextEditorInstance.Text; }
+            set
+            {
+                TextEditorInstance.Text = value;
+            }
+        }
+
+        private ICSharpCode.AvalonEdit.TextEditor TextEditorInstance => (this.elementHost1.Child as TextEditor).avalonTextEditor;
+
+        private void FrmMain_Shown(object sender, EventArgs e)
+        {
+            this.TextEditorInstance.Focus();
+        }
+    }
+}
