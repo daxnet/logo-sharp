2f7ef91d0fa317484b1c3d3913c29c17d94f5f5f
daxnet <daxnet@outlook.com>
2019-07-23 13:40:26 +0800

    Adding the LogoSharp IDE solution.

---
+++ ide/LogoSharp.IDE/FrmMain.cs
@@ -0,0 +1,21 @@
+﻿using FormsUI.Windows;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogoSharp.IDE
+{
+    public partial class FrmMain : AppWindow
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
